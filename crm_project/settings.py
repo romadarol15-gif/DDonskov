b6d36f1ab9a8ea111cbc1bb5ec7e378f78727a9a
romadarol15-gif <roma.darol15@gmail.com>
2026-02-23 00:56:03 +0300

    Fix LOGIN_URL redirect issue in settings

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -64,6 +64,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'tasks_app.User'
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
